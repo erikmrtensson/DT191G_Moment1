e3dfaeae43328a99bfe6ab3d4dfa7419f881618b
erikmrtensson <42966182+erikmrtensson@users.noreply.github.com>
2025-03-05 14:47:32 +0100

    Initial commit

---
+++ Uppgift1-1/Uppgift1-1/wwwroot/js/function.js
@@ -0,0 +1,14 @@
+﻿const colors = ["AliceBlue", "BlanchedAlmond", "FloralWhite", "GhostWhite", "White", "PaleGreen", "Cornsilk"];
+
+const pContent = document.getElementById('currentColor');
+
+const contentClass = document.getElementsByClassName('content')[0];
+
+document.getElementById('changeColorButton').addEventListener('click', function () {
+    let random = Math.floor(Math.random() * colors.length);
+    let newColor = colors[random];
+    contentClass.style.backgroundColor = newColor;
+
+    pContent.innerHTML = "Bakgrundsfärg: \"" + newColor + "\"";
+});
+
